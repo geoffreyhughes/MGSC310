8060f3fe5d8e69f8fd9cbd3cc466647a5ee624ca
Geoffrey Hughes <46589744+geoffreyhughes@users.noreply.github.com>
2021-01-05 12:18:03 -0800

    Problem Sets from Statistical Models in Business

---
+++ ProblemSet1_geoffreyhughes.R
@@ -0,0 +1,31 @@
+# Problem Set 1 R code - will put into the Rmd file to turn in
+# Geoffrey Hughes
+
+
+install.packages("tidyverse")
+library("tidyverse")
+
+
+# a.
+getwd()
+setwd("/Users/geoffreyhughes/Documents/MGSC_310/MGSC310")
+
+
+# b.
+movies <- read.csv("Datasets/movie_metadata.csv")
+head(movies)
+
+
+# c.
+dim(movies)
+# 5043 observations with 28 variables
+
+
+# d.
+names(movies)
+#
+
+
+
+
+
